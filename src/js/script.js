f5130f93d6691d0ae25f73ff98c3eeed81b6758b
”Enne” <EnneFofe10@gmail.com>
2023-10-28 13:51:30 -0300

    Organização de arquivos

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Chamada de todos os inputs e divs de aviso que serão usados
 const nome = window.document.getElementById('nome')
 const email = window.document.getElementById('email')
 const conemail = window.document.getElementById('conemail')
@@ -9,7 +10,9 @@ const aviso_conemail = window.document.getElementsByClassName('aviso')[2]
 const aviso_senha = window.document.getElementsByClassName('aviso')[3]
 const aviso_consenha = window.document.getElementsByClassName('aviso')[4]
 
-function limpar() {
+// Funcionalidade de limpar todo o formulário
+const limpar = document.getElementById('limpar')
+limpar.addEventListener('click', () => {
     nome.value = ''
     nome.innerHTML = ''
     email.value = ''
@@ -26,45 +29,55 @@ function limpar() {
     aviso_senha.innerHTML = ''
     aviso_consenha.innerHTML = ''
     alert('Tudo limpo com sucesso!')
-}
-function enviar() {
+})
+
+// Funcionalidadde de enviar todos os dados do formulário
+const enviar = document.getElementById('enviar')
+enviar.addEventListener('click', () => {
     if (consenha.value == '') {
         aviso_consenha.innerHTML = 'Preencha a confirmação de senha*'
         consenha.focus()
     } else {
         aviso_consenha.innerHTML = ''
     }
+
     if (senha.value == '') {
         aviso_senha.innerHTML = 'Preencha a senha*'
         senha.focus()
     } else {
         aviso_senha.innerHTML = ''
     }
+
     if (conemail.value == '') {
         aviso_conemail.innerHTML = 'Preencha a confirmação de email*'
         conemail.focus()
     } else {
         aviso_conemail.innerHTML = ''
     }
+
     if (email.value == '') {
         aviso_email.innerHTML = 'Preencha o email*'
         email.focus()
     } else {
         aviso_email.innerHTML = ''
     }
+
     if (nome.value == '') {
         aviso_nome.innerHTML = 'Preencha o nome*'
         nome.focus()
     } else {
         aviso_nome.innerHTML = ''
     }
+
     if (nome.value == '' || email.value == '' || conemail.value == '' || senha.value == '' || consenha.value == '') {
         window.alert('Faltam dados! Preencha o que estiver faltando.')
     } else {
         if (email.value != conemail.value) {
             alert('A confirmação de EMAIL precisa ser IGUAL!')
+
         } else if (senha.value != consenha.value) {
             alert('A confirmação de SENHA precisa ser IGUAL!')
+            
         } else {
             alert('Formulário preenchido com suceso!')
             nome.value = ''
@@ -79,4 +92,4 @@ function enviar() {
             consenha.innerHTML = ''
         }
     }
-}
+})
