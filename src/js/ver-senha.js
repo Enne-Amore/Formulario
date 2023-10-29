42424189f1fcad60de67710c9fadf442ae788164
”Enne” <EnneFofe10@gmail.com>
2023-10-29 17:24:44 -0300

    Senha
    Adicionando um botão de ver a senha realmente.

---
+++ src/js/ver-senha.js
@@ -0,0 +1,26 @@
+for (i = 0 ; i <= 1 ; i++) {
+    let senha_visivel = document.getElementsByClassName('visivel')[i]
+    let senha_invisivel = document.getElementsByClassName('invisivel')[i]
+    let password = document.getElementsByClassName('password')[i]
+
+    senha_visivel.addEventListener('click', () => {
+        if (senha_visivel.style.display == 'inline' && senha_invisivel.style.display == 'none') {
+            senha_visivel.style.display = 'none'
+            senha_invisivel.style.display = 'inline'
+            password.type = 'text'
+        } else {
+            senha_visivel.style.display = 'inline'
+            senha_invisivel.style.display = 'none'
+            password.type = 'password'
+        }
+    })
+    
+    senha_invisivel.addEventListener('click', () => {
+        if (senha_visivel.style.display == 'none' && senha_invisivel.style.display == 'inline') {
+            senha_visivel.style.display = 'inline'
+            senha_invisivel.style.display = 'none'
+            password.type = 'password'
+        }
+    })
+
+}
