918ac25cacf3db40230169cb0a8308b9e89be86a
”Enne” <EnneFofe10@gmail.com>
2023-11-04 15:07:26 -0300

    Acessibilidade
    Adicionando a possibilidade de poder ver realmente a senha usado apenas a tecla "enter" do teclado.

--- src/js/script.js
+++ src/js/script.js
@@ -13,83 +13,89 @@ const aviso_consenha = window.document.getElementsByClassName('aviso')[4]
 // Funcionalidade de limpar todo o formulário
 const limpar = document.getElementById('limpar')
 limpar.addEventListener('click', () => {
-    nome.value = ''
-    nome.innerHTML = ''
-    email.value = ''
-    email.innerHTML = ''
-    conemail.value = ''
-    conemail.innerHTML = ''
-    senha.value = ''
-    senha.innerHTML = ''
-    consenha.value = ''
-    consenha.innerHTML = ''
-    aviso_nome.innerHTML = ''
-    aviso_email.innerHTML = ''
-    aviso_conemail.innerHTML = ''
-    aviso_senha.innerHTML = ''
-    aviso_consenha.innerHTML = ''
-    alert('Tudo limpo com sucesso!')
+    const ok = confirm("Tem certeza que deseja apagar todos os dados já informados?")
+    if (ok) {
+        nome.value = ''
+        nome.textContent = ''
+        email.value = ''
+        email.textContent = ''
+        conemail.value = ''
+        conemail.textContent = ''
+        senha.value = ''
+        senha.textContent = ''
+        consenha.value = ''
+        consenha.textContent = ''
+        aviso_nome.textContent = ''
+        aviso_email.textContent = ''
+        aviso_conemail.textContent = ''
+        aviso_senha.textContent = ''
+        aviso_consenha.textContent = ''
+        alert('Tudo limpo com sucesso!')
+    }
 })
 
 // Funcionalidadde de enviar todos os dados do formulário
 const enviar = document.getElementById('enviar')
 enviar.addEventListener('click', () => {
-    if (consenha.value == '') {
-        aviso_consenha.innerHTML = 'Preencha a confirmação de senha*'
-        consenha.focus()
-    } else {
-        aviso_consenha.innerHTML = ''
-    }
-
-    if (senha.value == '') {
-        aviso_senha.innerHTML = 'Preencha a senha*'
-        senha.focus()
-    } else {
-        aviso_senha.innerHTML = ''
-    }
-
-    if (conemail.value == '') {
-        aviso_conemail.innerHTML = 'Preencha a confirmação de email*'
-        conemail.focus()
-    } else {
-        aviso_conemail.innerHTML = ''
-    }
-
-    if (email.value == '') {
-        aviso_email.innerHTML = 'Preencha o email*'
-        email.focus()
-    } else {
-        aviso_email.innerHTML = ''
-    }
-
-    if (nome.value == '') {
-        aviso_nome.innerHTML = 'Preencha o nome*'
-        nome.focus()
-    } else {
-        aviso_nome.innerHTML = ''
-    }
-
-    if (nome.value == '' || email.value == '' || conemail.value == '' || senha.value == '' || consenha.value == '') {
-        window.alert('Faltam dados! Preencha o que estiver faltando.')
-    } else {
-        if (email.value != conemail.value) {
-            alert('A confirmação de EMAIL precisa ser IGUAL!')
-
-        } else if (senha.value != consenha.value) {
-            alert('A confirmação de SENHA precisa ser IGUAL!')
-            
+    const ok = confirm("Tem certeza que deseja enviar todos os dados já informados?")
+    if (ok) {
+        if (consenha.value == '') {
+            aviso_consenha.textContent = 'Preencha a confirmação de senha*'
+            consenha.focus()
         } else {
-            alert('Formulário preenchido com suceso!')
-            nome.value = ''
-            nome.innerHTML = ''
-            email.value = ''
-            email.innerHTML = ''
-            conemail.value = ''
-            conemail.innerHTML = ''
-            senha.value = ''
-            senha.innerHTML = ''
-            consenha.value = ''
-            consenha.innerHTML = ''
+            aviso_consenha.textContent = ''
+        }
+
+        if (senha.value == '') {
+            aviso_senha.textContent = 'Preencha a senha*'
+            senha.focus()
+        } else {
+            aviso_senha.textContent = ''
+        }
+
+        if (conemail.value == '') {
+            aviso_conemail.textContent = 'Preencha a confirmação de email*'
+            conemail.focus()
+        } else {
+            aviso_conemail.textContent = ''
+        }
+
+        if (email.value == '') {
+            aviso_email.textContent = 'Preencha o email*'
+            email.focus()
+        } else {
+            aviso_email.textContent = ''
+        }
+
+        if (nome.value == '') {
+            aviso_nome.textContent = 'Preencha o nome*'
+            nome.focus()
+        } else {
+            aviso_nome.textContent = ''
+        }
+
+        if (nome.value == '' || email.value == '' || conemail.value == '' || senha.value == '' || consenha.value == '') {
+            window.alert('Faltam dados! Preencha o que estiver faltando.')
+        } else {
+            if (email.value != conemail.value) {
+                alert('A confirmação de EMAIL precisa ser IGUAL!')
+
+            } else if (senha.value != consenha.value) {
+                alert('A confirmação de SENHA precisa ser IGUAL!')
+                
+            } else {
+                alert('Formulário preenchido com suceso!')
+                nome.value = ''
+                nome.textContent = ''
+                email.value = ''
+                email.textContent = ''
+                conemail.value = ''
+                conemail.textContent = ''
+                senha.value = ''
+                senha.textContent = ''
+                consenha.value = ''
+                consenha.textContent = ''
+            }
         }
     }
 })
--- src/js/ver-senha.js
+++ src/js/ver-senha.js
@@ -23,4 +23,19 @@ for (i = 0 ; i <= 1 ; i++) {
         }
     })
 
+    // Ver senha usando o teclado
+    senha_visivel.addEventListener('keypress', (tecla) => {
+        if (tecla.key === "Enter") {
+            tecla.target.click()
+            senha_invisivel.focus()
+        }
+    })
+    
+    senha_invisivel.addEventListener('keypress', (tecla) => {
+        if (tecla.key === "Enter") {
+            tecla.target.click()
+            senha_visivel.focus()
+        }
+    })
+
 }
